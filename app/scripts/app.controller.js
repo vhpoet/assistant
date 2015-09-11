4f7d5d401e5b6f38047441a5524be50c7ee81669
Vahe Hovhannisyan <vhpoet@gmail.com>
2015-09-11 14:02:45 -0700

    First feature. paste json to open it in external window

--- app/scripts/app.controller.js
+++ app/scripts/app.controller.js
@@ -4,9 +4,17 @@ angular
   .module('app')
   .controller('AppController', AppController);
 
-AppController.$inject = ['$rootScope'];
+AppController.$inject = ['$scope'];
 
 function AppController ($scope)
 {
+  $scope.$watch('theInput', function(value){
+    try {
+      JSON.parse(value);
 
+      window.open('http://www.jsoneditoronline.org/?json=' + escape(value),'_blank');
+    } catch (e) {
+      return false;
+    }
+  })
 }
